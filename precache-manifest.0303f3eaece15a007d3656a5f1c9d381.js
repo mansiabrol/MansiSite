3c756add27738a26a81b934b838450574a144a4f
mansiabrol <69516002+mansiabrol@users.noreply.github.com>
2020-09-13 13:37:00 +0530

    Updates

---
+++ precache-manifest.0303f3eaece15a007d3656a5f1c9d381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbcd613d144f6008f54262bf495fd840",
+    "url": "/MansiSite/index.html"
+  },
+  {
+    "revision": "d6bda6037abe407a1803",
+    "url": "/MansiSite/static/css/main.d6247299.chunk.css"
+  },
+  {
+    "revision": "f06ef528fa26571d2fcd",
+    "url": "/MansiSite/static/js/2.c117278f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MansiSite/static/js/2.c117278f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bda6037abe407a1803",
+    "url": "/MansiSite/static/js/main.8e16e2da.chunk.js"
+  },
+  {
+    "revision": "2aa87c960f3e36be0067",
+    "url": "/MansiSite/static/js/runtime-main.ad1d5a17.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MansiSite/static/media/logo.5d5d9eef.svg"
+  }
+]);
